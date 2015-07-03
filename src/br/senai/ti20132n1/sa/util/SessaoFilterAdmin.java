12ffd0876f8bd733b545609046e2e5ad6749ecc2
bruno_schmitt <bruno_schmitt@SNFNS2EP01VM060.florianopolis.sc.senai.br>
2015-07-03 18:43:51 -0300

    oi

---
+++ src/br/senai/ti20132n1/sa/util/SessaoFilterAdmin.java
@@ -0,0 +1,43 @@
+package br.senai.ti20132n1.sa.util;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import br.senai.sc.ti20132n1.sa.mb.SessaoAdminMB;
+
+@WebFilter(urlPatterns = "/admin/*")
+public class SessaoFilterAdmin implements Filter{
+	
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response,
+			FilterChain filterChain) throws IOException, ServletException {
+		HttpServletRequest servletRequest = (HttpServletRequest) request;
+		SessaoAdminMB sessaoAdminMB = (SessaoAdminMB) servletRequest.getSession()
+				.getAttribute("sessaoAdminMB");
+
+		if (sessaoAdminMB == null || !sessaoAdminMB.isLogadoAdmin()) {
+			HttpServletResponse servletResponse = (HttpServletResponse) response;
+			servletResponse.sendRedirect(servletRequest.getContextPath()
+					.concat("/login.xhtml"));
+		}
+		filterChain.doFilter(request, response);
+	}
+
+	@Override
+	public void destroy() {
+	}
+
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+	}
+
+}
